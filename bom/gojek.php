d52e969b328b791e00fc493073ee86d1f8506d0d
ibnuabbas9 <54257784+ibnuabbas9@users.noreply.github.com>
2019-08-19 11:55:57 +0700

    Update gojek.php

--- bom/gojek.php
+++ bom/gojek.php
@@ -9,8 +9,8 @@ include 'func.php';
 $init = new Bom();
 
 //Eksekusi Call/Sms Boomber (Limit 3x/Jam)
-$init->type = 2; //Type 2 untuk telpon, Type 1 untuk sms
-$init->no = "081220777880"; //Nomer Hp tujuan
+$init->type = 1; //Type 2 untuk telpon, Type 1 untuk sms
+$init->no = "082214651837"; //Nomer Hp tujuan
 
 if ($init->type == 1) {
 	for ($i=0; $i < 2; $i++) { 
